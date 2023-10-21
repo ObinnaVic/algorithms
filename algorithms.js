115d6253e59ada75824f5473f1b9e13d4e2d1911
Victor Nkire <nkirevictor77@gmail.com>
2023-10-21 23:15:39 +0100

    Queue data structure

--- algorithms.js
+++ algorithms.js
@@ -1,3 +1,7 @@
+//-------------------------------------------------------ALGORITHMS-----------------------------------------------------
+
+
+
 //FINACCI SEQUENCE
 
 //Question 1: Given a number "n", find the first "n" elements of the fibonacci sequence:
@@ -104,7 +108,7 @@ console.log(recursiveFib(10));
 const linSearch = (n, t) => {
     for (let i = 0; i < n.length; i++) {
         if (n[i] == t) {
-            return result = i;
+            return i;
         } 
     }
     return -1
@@ -277,3 +281,364 @@ const sortArr = (arr) => {
 }
 
 console.log(sortArr([2, -3, 1, -8, 5, 7, 4, 9]));
+
+
+
+//CARTESIAN PRODUCT
+
+const cartesianProduct = (arr1, arr2) => {
+    let result = [];
+    for (const i of arr1) for (const j of arr2) result.push([i, j]) 
+//-------- O(m*n) this is because the two arrays are of different 
+//size and solutio is dependent on the length of the two arrays
+
+    return result;
+}
+
+console.log(cartesianProduct([1, 3, 4], [7, 6, 8]));
+
+
+///Find the longest "n" unique substring of a string where "n" begins the string i.e "2aabbacbaa"; 
+
+const longestSubString = (str) => {
+    let n = +str[0];
+    let string = str.slice(1);
+    
+    const generateSubStrings = (sub_str) => {
+        let subStr = [];
+        for (let i = 0; i < sub_str.length; i++) {
+            
+            subStr.push(sub_str[i]);
+
+            for (let j = i + 1; j < sub_str.length; j++) {
+
+                subStr.push(subStr[subStr.length - 1] + sub_str[j])
+                
+            }
+            
+        }
+        return subStr
+    }
+
+    console.log(generateSubStrings(string));
+
+    const uniqueSubstr = generateSubStrings(string).filter((str) => {
+        let unique = []
+
+        for (const element of str) {
+            
+            if (!unique.includes(element)) {
+                
+                unique.push(element)
+            
+            }
+
+        }
+
+        return unique.length === n
+    }) 
+    
+    console.log(uniqueSubstr);
+
+    const longestSubStr = (strArr) => {
+        return strArr.reduce((acc, cur) => cur.length > acc.length ? cur : acc, strArr[0])
+    }
+
+    return `The longest ${n} unique alphabets substring is ${longestSubStr(uniqueSubstr)}`
+
+}
+
+console.log(longestSubString("2aabbacbaa"))
+
+
+
+//-------------------------------------------------------DATA STRUCTURES-----------------------------------------------------
+
+//STACK
+//A stack is a collection of elements that follows the LIFO principle. That is, the LAST IN, FIRST OUT, 
+//where by the last element added to a stack is the first element that can the removed from the stack.
+
+class Stack {
+    constructor() {
+        this.items = [];
+    }
+
+
+    push(element) { //This is to add an element from the stack data
+        this.items.push(element);
+    }
+
+
+    pop() { ///This is to remove and return the last added element from the collection.
+        return this.items.pop();
+    }
+
+
+    peek() { ///This is to return the last element in the collection or top-most element in the stack
+        return this.items[this.items.length - 1];
+    }
+
+
+    isEmpty() {
+        return this.items.length === 0;
+    }
+
+
+    size() {
+        return this.items.length;
+    }
+
+
+    print() {
+        console.log(this.items.toString());
+    }
+
+
+
+}
+
+const stack = new Stack();
+
+console.log(stack.isEmpty());
+
+stack.push(10);
+stack.push(20);
+stack.push(30);
+
+console.log(stack.isEmpty());
+
+console.log(stack.peek());
+
+console.log(stack.pop());
+
+console.log(stack.peek());
+
+stack.print()
+
+
+
+//QUEUE DATA STRUCTURE
+//This is a sequential collection of elements that follows the principle of Firts In, FIrst Out. 
+//That is, the first element added to the queue is also the first element that is removed from the queue.
+
+class Queue {
+    constructor() {
+        this.items = [];
+    }
+
+
+    enqueue(element) {
+        this.items.push(element);
+    }
+
+
+    dequeue() { /////////// --------- O(n)
+        return this.items.shift();
+    }
+
+
+    peek() {
+        return this.items[0];
+    }
+
+
+    isEmpty(){
+        return this.items.length === 0;
+    }
+
+
+    size() {
+        return this.items.length
+    }
+
+
+    print() {
+        return this.items.toString();
+    }
+}
+
+const queue = new Queue;
+
+console.log(queue.isEmpty());
+
+queue.enqueue(10);
+queue.enqueue(20);
+queue.enqueue(30);
+
+console.log(queue.size());
+
+console.log(queue.isEmpty());
+
+console.log(queue.peek());
+
+console.log(queue.dequeue());
+
+console.log(queue.peek());
+
+console.log(queue.print());
+
+
+//More Optimized way of creating and using a queue: This is by using object instead of Array
+
+class Queue2 {
+    constructor() {
+        this.items = {};
+        this.rear = 0;
+        this.front = 0;
+    }
+
+    enqueue(element) {
+        this.items[this.rear] = element;
+        this.rear++;
+    }
+
+
+    dequeue() { /////////// --------- O(1)
+        const item = this.items[this.front];
+        delete(this.items[this.front]);
+        this.front++
+        return item;
+    }
+
+
+    peek() {
+        return this.items[this.front];
+    }
+
+
+    isEmpty() {
+        return this.rear - this.front === 0;
+    }
+
+
+    size() {
+        return this.rear - this.front;
+    }
+
+
+    print() {
+        return this.items;
+    }
+}
+
+
+const queue2 = new Queue2;
+
+console.log(queue2.isEmpty());
+
+queue2.enqueue(10);
+queue2.enqueue(20);
+queue2.enqueue(30);
+
+console.log(queue2.isEmpty());
+
+console.log(queue2.peek());
+console.log(queue2.dequeue());
+console.log(queue2.peek());
+
+console.log(queue2.size());
+console.log(queue2.print());
+
+
+
+
+////CIRCULAR QUEUE
+//Here, the size of the queue is fixed and it works as if the first element is connected to the last element.
+//Also referred to as the ring buffer or circular buffer and aslo follows the principle of FIFO.
+
+class CircularQueue {
+    constructor(capacity) { //capacity is the declared fixed size of the array.
+        this.items = new Array(capacity);
+        this.capacity = capacity;
+        this.currentLength = 0;
+        this.rear = -1;
+        this.front = -1;
+    }
+
+
+    isFull() {
+        return this.capacity === this.currentLength;
+    }
+
+    isEmpty() {
+        return this.currentLength === 0;
+    }
+
+
+    enqueue(element) {
+        if (this.isEmpty()) {
+            this.front += 1
+        }
+        if (!this.isFull()) {
+            this.rear = (this.rear + 1) % this.capacity;
+            this.currentLength = this.currentLength + 1;
+            this.items[this.rear] = element;
+        } else {
+            return "Queue is full";
+        }
+    }
+
+
+    dequeue() {
+        if (this.isEmpty()) {
+            return null;
+        }
+        let item = this.items[this.front];
+        this.items[this.front] = null;
+        this.front = (this.front + 1) % this.capacity
+        this.currentLength = this.currentLength - 1;
+        
+        if (this.isEmpty()) {
+            this.rear = -1;
+            this.front = -1;
+        }
+        return item;
+    }
+
+
+    peek() {
+        if (!this.isEmpty()) {
+            return this.items[this.front];
+        }
+        return null;
+    }
+
+
+    print() {
+        let i
+        let str = "";
+        for(i = this.front; i !== this.rear; i = (i+1)%this.capacity) {
+            str+= this.items[i] + " ";
+        }
+
+        str+= this.items[i];
+        return str;
+    }
+
+    size() {
+        return this.currentLength;
+    }
+}
+
+const circularQueue = new CircularQueue(5);
+
+console.log(circularQueue.isEmpty());
+console.log(circularQueue.isFull());
+
+circularQueue.enqueue(10);
+circularQueue.enqueue(20);
+circularQueue.enqueue(30);
+circularQueue.enqueue(40);
+circularQueue.enqueue(50);
+
+console.log(circularQueue.isEmpty());
+console.log(circularQueue.isFull());
+
+console.log(circularQueue.print());
+
+circularQueue.dequeue();
+
+console.log(circularQueue.print());
+circularQueue.enqueue(60);
+console.log(circularQueue.print());
+console.log(circularQueue.size());
